902333fc05dd4ba050692a5f66037496e3c7f597
cidherp2 <alexlarios70@hotmail.com>
2026-09-19 20:06:59 -0600

    test: añade smoke test del esquema de personas

---
+++ supabase/tests/persons.sql
@@ -0,0 +1,13 @@
+begin;
+
+select plan(6);
+
+select has_table('public', 'persons', 'La tabla persons existe');
+select col_not_null('public', 'persons', 'name', 'El nombre es obligatorio');
+select col_not_null('public', 'persons', 'email', 'El correo es obligatorio');
+select col_type_is('public', 'persons', 'phone', 'character varying', 'El telefono es varchar');
+select is((select relrowsecurity from pg_class where oid = 'public.persons'::regclass), true, 'RLS esta habilitado');
+select is((select count(*) from pg_policies where schemaname = 'public' and tablename = 'persons'), 4::bigint, 'Existen cuatro politicas autenticadas');
+
+select * from finish();
+rollback;
